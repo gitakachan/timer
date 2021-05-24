853497f6c6315770a204b3cab0974200b24fddb8
Cheni Chen <celiachenic@gmail.com>
2021-05-25 00:46:12 +0800

    設置基本計時功能

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-window.onload = function(){
-
-    
+//簡化getElementById
+function $(id) {
+  return document.getElementById(id);
 }
+
+window.onload = function () {
+  let hr = $("hr");
+  let min = $("min");
+  let sec = $("sec");
+  let start = $("start");
+  let pause = $("pause");
+  let reset = $("reset");
+
+  //開始計時
+  let count = 0; //開始計時後累加的總秒數
+  let timer = null;
+  start.onclick = function () {
+    timer = setInterval(function () {
+      count++;
+      //根據count來改變顯示的時分秒的值
+      sec.innerHTML = count % 60;
+      min.innerHTML = parseInt(count / 60) % 60;
+      hr.innerHTML = parseInt(count / 60 / 60);
+    }, 1000);
+  };
+
+  console.log(start);
+};
